3322865187fe1ccdd6770a92ed9fcbf3e603ce5d
totuoithan <manhmanhx008@gmail.com>
2024-01-12 23:33:54 +0700

    Create Project

--- Java1/src/App.java
+++ Java1/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Ngồi gần 4 tiếng mới mò đc cách làm ");
     }
 }
