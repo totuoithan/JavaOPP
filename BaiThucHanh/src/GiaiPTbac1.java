cfc09bfed41851686a1d5ab731c1abac287fe9f8
totuoithan <manhmanhx008@gmail.com>
2024-01-19 11:01:21 +0700

    Phuongtrinhbac1

---
+++ BaiThucHanh/src/GiaiPTbac1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class GiaiPTbac1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double a, b, x;
+        System.out.println(" Nhap a: ");
+        a= sc.nextDouble();
+        System.out.println(" Nhap b: ");
+        b= sc.nextDouble();
+        if(a == 0){
+            if(b == 0){
+                System.out.println(" PT co vo so nghiem");
+            } else{
+                System.out.println(" PT vo nghiem");
+            }
+        } else{
+            x= -b/a;
+            System.out.println("x= "+x);
+        }
+    }
+}
