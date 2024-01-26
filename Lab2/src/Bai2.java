48c6c711b37379b16d5e8f383f954a66ae1df270
totuoithan <manhmanhx008@gmail.com>
2024-01-26 19:20:03 +0700

    Bai2

--- Lab2/src/Bai2.java
+++ Lab2/src/Bai2.java
@@ -4,6 +4,7 @@ public class Bai2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("nhap 1 so: ");
+        
         int so;
         so = sc.nextInt();
         if(so % 2 == 0){
