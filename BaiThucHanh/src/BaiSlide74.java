1baaf3d9321220d9e5f1d80411d3724b73cd9ecf
totuoithan <manhmanhx008@gmail.com>
2024-02-02 23:04:25 +0700

    Slide74

--- BaiThucHanh/src/BaiSlide74.java
+++ BaiThucHanh/src/BaiSlide74.java
@@ -2,22 +2,25 @@ import java.util.Scanner;
 
 public class BaiSlide74 {
     public static void main(String[] args) {
+        int n;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Nhap so luong phan tu cua mang: ");
-        int n = sc.nextInt();
-        int[] mang = new int[n];
-        System.out.println("Nhap gia tri cho mang:");
-        for (int i = 0; i < n; i++) {
-            System.out.print("nhap phan tu thu " + (i + 1) + ": ");
-            mang[i] = sc.nextInt();
+        do{
+            System.out.println(" nhap vao so phan tu cua mang: ");
+            n = sc.nextInt();
+        } while(n<0);
+        int A[] = new int[n];
+        System.out.println(" nhao cac phan tu cho mang: ");
+        for(int i = 0; i < n; i++){
+            System.out.print(" nhap phan tu thu " + i +" vao mang: ");
+            A[i]= sc.nextInt();
         }
-        int tongSoChan = 0;
-        for (int i = 0; i < n; i++) {
-            if (mang[i] % 2 == 0) {
-                tongSoChan = tongSoChan + mang[i];
+        double Tong = 0;
+        for(int i =0; i < n; i++){
+            if(A[i] % 2 == 0){
+                Tong = Tong + A[i];
             }
         }
-        System.out.println("Tong cua mang la:  " + tongSoChan);
+        System.out.println("Tong cua mang la: " + Tong);
         sc.close();
     }
 }
