c5f4e0bdaddf0321cfc7eb3067d333aa8fb8fcca
totuoithan <manhmanhx008@gmail.com>
2024-04-13 14:59:13 +0700

    Slide 80 lab3

--- BaiThucHanh/src/model/MainClass.java
+++ BaiThucHanh/src/model/MainClass.java
@@ -6,9 +6,9 @@ public class MainClass {
     public static void main(String[] args) {
         NhanVienFulltime sep = new NhanVienFulltime("Nguyen Van A ", 4);
         sep.setloaiChucVu((int)Configs.NHAN_VIEN_SEP);
-        NhanVienFulltime linh1 = new NhanVienFulltime("Tran Van B ", 7);
+        NhanVienFulltime linh1 = new NhanVienFulltime("Tran Van B ", 7);// Nhan vien lam them ngay
         linh1.setloaiChucVu((int)Configs.NHAN_VIEN_LINH);
-        NhanVienFulltime linh2 = new NhanVienFulltime("Le Thi C");
+        NhanVienFulltime linh2 = new NhanVienFulltime("Le Thi C");// Nhan vien khong lam them ngay
         linh2.setloaiChucVu((int)Configs.NHAN_VIEN_LINH);
         NhanVienPartTime Thoivu = new NhanVienPartTime("Le Van D", 240);
 
