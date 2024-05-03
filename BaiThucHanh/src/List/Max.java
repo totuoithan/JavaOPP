bd3934e64d2861b2000b777146b53b9ddf3ea6c4
totuoithan <manhmanhx008@gmail.com>
2024-05-03 11:02:43 +0700

    Slide25 Lab4

---
+++ BaiThucHanh/src/List/Max.java
@@ -0,0 +1,25 @@
+package List;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Max {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrListInteger = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int number;
+        System.out.println(" Nhap so phan tu cua ArrayList: ");
+        int n = sc.nextInt();
+        for(int i =0; i< n; i++){
+            System.out.println("Nhap phan tu thu " + i + " : ");
+            number = sc.nextInt();
+            arrListInteger.add(number);
+        }
+        int max = arrListInteger.get(0);
+        for(int i =1; i< arrListInteger.size(); i++){
+            if(arrListInteger.get(i).compareTo(max)>0){
+                max = arrListInteger.get(i);
+            }
+        }
+        System.out.println("Phan tu lon nhat trong arrListInteger la: " + max);
+    }        
+}
