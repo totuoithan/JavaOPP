0511ee5b1591726e4b2ded26b39db17da4b60b32
totuoithan <manhmanhx008@gmail.com>
2024-05-23 14:46:08 +0700

    Bai1 Lab4

---
+++ Lab4/src/Bai1.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        System.out.print("Nhap so phan tu: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print("Nhap gia tri phan tu thu: " + (i + 1) + ": ");
+            int number = sc.nextInt();
+            arrayList.add(number);
+        }
+        int max = Collections.max(arrayList);
+        System.out.println("Phan tu lon nhat trong day la: " + max);
+        System.out.println("Nhap gia tri muon xoa: ");
+        int remove = sc.nextInt();
+        arrayList.removeIf(number -> number == remove);
+        System.out.println("List sau khi xoa: " +arrayList);
+        Collections.sort(arrayList);
+        System.out.println("List sau khi sap xep: " + arrayList);
+
+    }
+}
